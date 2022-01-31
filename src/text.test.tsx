dc4e66727f597033805dec75023b58fbb3230923
Austin Cory Bart <acbart@vt.edu>
2022-01-31 16:41:17 -0500

    Rename text.Test.tsx to text.test.tsx

--- src/text.test.tsx
+++ src/text.test.tsx
@@ -4,6 +4,11 @@ import App from "./App";
 
 test("renders the text 'Hello World' somewhere", () => {
     render(<App />);
+<<<<<<< HEAD
     const texts = screen.getAllByText(/Hello World/);
     expect(texts.length).toBeGreaterThanOrEqual(1);
+=======
+    const text = screen.getByText(/Hello World/);
+    expect(text).toBeInTheDocument();
+>>>>>>> 375a37b (Rename text.Test.tsx to text.test.tsx)
 });
