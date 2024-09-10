b4c337b5f1a681fd4b5409eda814be8e8b663459
sidhb23 <siddhartha2002@hotmail.com>
2024-09-10 01:03:25 -0400

    Hello World

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App(): React.JSX.Element {
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Siddhartha Bajracharya
+                automatically reload. Siddhartha Bajracharya. Hello World.
             </p>
         </div>
     );
