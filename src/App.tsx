ee10a315f6c81ac747af517eb0e5f8eae41d92bc
sidhb23 <siddhartha2002@hotmail.com>
2024-11-09 17:57:13 -0500

    solved the components

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App(): React.JSX.Element {
                 UM COS420 with React Hooks and TypeScript
             </header>
             <hr></hr>
-            {/* <DoubleHalf></DoubleHalf> */}
+            {<DoubleHalf></DoubleHalf>}
             <hr></hr>
             <ChooseTeam></ChooseTeam>
             <hr></hr>
@@ -29,61 +29,6 @@ function App(): React.JSX.Element {
             <TwoDice></TwoDice>
             <hr />
             <ChangeType></ChangeType>
-
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.{" "}
-                <span style={{ color: "red" }}>Siddhartha Bajrachary</span>
-            </p>
-
-            <Container>
-                <Row>
-                    <Col>
-                        <div
-                            style={{
-                                width: "100px", // Adjust width as needed
-                                height: "100px", // Adjust height as needed
-                                backgroundColor: "red",
-                                marginBottom: "10px", // Optional margin
-                            }}
-                        ></div>
-                        <div
-                            style={{ border: "3px solid blue", padding: "2px" }}
-                        >
-                            <p>Bordered Stuff</p>
-                        </div>
-                        Unordered List:
-                        <ol>
-                            <li>Uno</li>
-                            <li>Dos</li>
-                            <li>Tres</li>
-                        </ol>
-                    </Col>
-
-                    <Col>
-                        <div
-                            style={{
-                                width: "100px", // Adjust width as needed
-                                height: "100px", // Adjust height as needed
-                                backgroundColor: "red",
-                                marginBottom: "10px", // Optional margin
-                            }}
-                        ></div>
-                        <img
-                            src={nyImage}
-                            alt="New York"
-                            style={{ width: "800px", height: "450px" }}
-                        />
-                        <Button
-                            onClick={() => {
-                                console.log("Hello World!");
-                            }}
-                        >
-                            Log Hello World
-                        </Button>
-                    </Col>
-                </Row>
-            </Container>
         </div>
     );
 }
